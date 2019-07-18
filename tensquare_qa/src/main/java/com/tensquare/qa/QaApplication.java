1a9ae4cafcf6000adbbcb303d43460e4686e6048
xielei <xl819202558@qq.com>
2019-07-18 18:12:23 +0800

    新增 问答模块 基础功能

---
+++ tensquare_qa/src/main/java/com/tensquare/qa/QaApplication.java
@@ -0,0 +1,19 @@
+package com.tensquare.qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：启动类
+ *
+ * @author xielei
+ * @date 2019/07/18
+ */
+
+@SpringBootApplication
+public class QaApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(QaApplication.class);
+    }
+}
