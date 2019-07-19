607a14d237a860872e66cb582b1374cced4aaddc
xielei <xl819202558@qq.com>
2019-07-19 10:52:03 +0800

    新增 文章模块 基础配置

---
+++ tensquare_article/src/main/java/com/tensquare/article/ArticleApplication.java
@@ -0,0 +1,19 @@
+package com.tensquare.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：启动类
+ *
+ * @author xielei
+ * @date 2019/07/19
+ */
+
+@SpringBootApplication
+public class ArticleApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class);
+    }
+}
